3b66d6475eef648daa23a30c0e20fe6594063832
andr3w1000 <andr3w1000@gmail.com>
2026-03-28 18:17:13 +0100

    add project related files to run the project

--- function_app.py
+++ function_app.py
@@ -11,25 +11,6 @@ from src.eventhub_service.azure_eventhub_service import AzureEventHubService
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="uploadFile", methods=["POST"])
-def upload_file(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    account_url = os.environ["STORAGE_ACCOUNT_URL"]
-    container_name = "landing"
-
-    file = req.files.get('file')
-    if file is None:
-        return func.HttpResponse("No file provided.", status_code=400)
-
-    try:
-        blob_service = AzureBlobService(account_url)
-        blob_service.upload_blob(container_name, file.filename, file.stream)
-        return func.HttpResponse(f"File {file.filename} uploaded successfully.", status_code=200)
-    except Exception as e:
-        logging.error(f"Error uploading file: {e}")
-        return func.HttpResponse(f"Error uploading file: {e}", status_code=500)
-
-
 @app.route(route="sendDummyUser", methods=["POST"])
 def send_dummy_user(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Generating dummy user data and sending to EventHub.")
